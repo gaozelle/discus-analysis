ce91ba150811740314572984effa9b5b3b2f7e25
Zhanle Gao <macgazelle@ZhanledeMacBook-Pro.local>
2018-04-17 22:18:45 +0800

    和速度与垂直速度T检验

--- t检验.R
+++ t检验.R
@@ -1,4 +1,5 @@
 # t检验
-
+score <- c(57.91,57.07,55.63,55.30,54.80,54.50,54.40,53.95)
  
-t.test(cyl$vdiscusR,cyl$vdiscusZ,paired = T)
+# t.test(`1tk57.91`$vdiscusR,`1tk57.91`$vdiscusZ,paired = T)
+cor(score,vshoot)
