93f1201bba5d65db0d9de161343d50950731be63
Zhanle Gao <macgazelle@gazelle.local>
2018-06-09 22:37:50 +0800

    保存选定的列为txt文件，方便与signalTEC数据比较误差

---
+++ 保存选定列并存为txt.R
@@ -0,0 +1,19 @@
+# 选择数据列，并改名加字母s
+no1tk57.91s<- no1tk57.91[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+no2zmj57.07s<-no2zmj57.07[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+no3ssc55.63s<- no3ssc55.63[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+no4wzd55.30s<- no4wzd55.30[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+no5tlg54.80s<- no5tlg54.80[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+no6whl54.50s<- no6whl54.50[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+no7cyl54.40s<- no7cyl54.40[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+no8hqb53.95s<- no8hqb53.95[c('v重心X','v重心Y','v重心Z','v重心R','vdiscusX','vdiscusY','vdiscusZ','vdiscusR')]
+# 改名后的数据保存为相应名称的txt文件
+write.table(no1tk57.91s,"/Users/macgazelle/Desktop/no1tk57.91s.txt")
+write.table(no2zmj57.07s,"/Users/macgazelle/Desktop/no2zmj57.07s.txt")
+write.table(no3ssc55.63s,"/Users/macgazelle/Desktop/no3ssc55.63s.txt")
+write.table(no4wzd55.30s,"/Users/macgazelle/Desktop/no4wzd55.30s.txt")
+write.table(no5tlg54.80s,"/Users/macgazelle/Desktop/no5tlg54.80s.txt")
+write.table(no6whl54.50s,"/Users/macgazelle/Desktop/no6whl54.50s.txt")
+write.table(no7cyl54.40s,"/Users/macgazelle/Desktop/no7cyl54.40s.txt")
+write.table(no8hqb53.95s,"/Users/macgazelle/Desktop/no8hqb53.95s.txt")
+write.table(whls,"/Users/macgazelle/Desktop/no8hqb53.95s.txt")
